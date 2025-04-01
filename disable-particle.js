1216e61da9b482bfe3a7fe8f2794a2949a1bcb20
kamata-bug-factory <kazukazuki517@gmail.com>
2025-04-01 16:06:20 +0900

    feat: リンクにホバーしたときポインタを表示する

--- disable-particle.js
+++ disable-particle.js
@@ -9,9 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
   // パーティクルのために非表示にされていたカーソルを表示する
   document.body.style.cursor = 'default';
 
-  const blockMousemove = (event) => {
-    event.stopImmediatePropagation();
-  };
+  const links = document.querySelectorAll('a');
+  const pointer = document.getElementById('POINTER');
 
-  window.addEventListener('mousemove', blockMousemove, true);
+  // aタグにmouseenterしたらPOINTERを表示、mouseleaveしたら非表示にする
+  // styleの各属性はオリジナルと同じものを設定する
+  links.forEach((link) => {
+    link.addEventListener('mouseenter', () => {
+      pointer.style.visibility = 'inherit';
+      pointer.style.opacity = '1';
+      pointer.style.transform = 'translate(-50%, -50%) scale(1.5, 1.5)';
+    });
+
+    link.addEventListener('mouseleave', () => {
+      pointer.style.visibility = 'hidden';
+      pointer.style.opacity = '0';
+      pointer.style.transform = 'translate(-50%, -50%) scale(1, 1)';
+    });
+  });
+
+  // マウスカーソルの動きに合わせてPOINTERを追従する
+  window.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    // スクロール位置を考慮する
+    const scrollY = window.scrollY;
+
+    // POINTERが表示されているとき、マウスカーソルの位置に追従させる
+    if (pointer.style.visibility === 'inherit') {
+      pointer.style.left = mouseX + 'px';
+      pointer.style.top = mouseY + scrollY + 'px';
+    }
+  });
 });
